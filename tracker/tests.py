e4e390b4858c2c1535f409783d0fbfeea2551804
Enapiuz <enapiuz@gmail.com>
2016-07-10 13:42:12 +0300

    some tracker tests

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,16 +1,24 @@
+import uuid
 from django.test import TestCase
 from .services import create_new_site_visitor
 from .models import Person, PersonVisit
 
 
 class PersonManageTestCase(TestCase):
-    def setUp(self):
-        pass
-
     def test_create_new_person(self):
-        uid = create_new_site_visitor().uid
-        count = Person.objects.filter(uid=uid).count()
-        self.assertEquals(count, 1)
+        old_count = Person.objects.count()
+        create_new_site_visitor()
+        new_count = Person.objects.count()
+        self.assertEquals(old_count + 1, new_count)
+
+    def test_person_uuid(self):
+        person = create_new_site_visitor()
+        has_error = False
+        try:
+            uuid.UUID(str(person.uid), version=1)
+        except Exception:
+            has_error = True
+        self.assertFalse(has_error)
 
     def test_serving_shiva_js(self):
         resp = self.client.get('/tracker/shiva.js')
@@ -21,8 +29,16 @@ class PersonManageTestCase(TestCase):
         succ_resp = self.client.get('/tracker/track', {'p': 'test_page'})
         self.assertEquals(succ_resp.status_code, 200)
         new_total_visits = PersonVisit.objects.count()
-        self.assertEquals(old_visits+1, new_total_visits)
+        self.assertEquals(old_visits + 1, new_total_visits)
 
     def test_client_visit_fail_log(self):
         fail_resp = self.client.get('/tracker/track')
         self.assertEquals(fail_resp.status_code, 403)
+
+    def test_additional_information(self):
+        resp = self.client.get('/tracker/attach', {'t': 'email', 'v': 'testmail@example.com'})
+        self.assertEquals(resp.status_code, 200)
+        uid = self.client.cookies.get('uid').value
+        count = Person.objects.filter(uid=uid, personinfo__info_type='email',
+                                      personinfo__info_value='testmail@example.com').count()
+        self.assertEquals(count, 1)
