8de20dfbfac6fba5716f8d6c4bf915bd8cad4e8a
Vadim Shashin <enapiuz@gmail.com>
2016-07-19 09:45:08 +0300

    Print debug :D

--- tracker/tasks.py
+++ tracker/tasks.py
@@ -6,6 +6,7 @@ from .models import PersonEvent, PersonData, PersonContact
 
 @app.task()
 def handle_background(caller_type, person_id, caller_name, caller_value):
+    # web github driven development :D
     from .services import get_or_create_person
     caller_type = int(caller_type)
     print("begin handle background!")
@@ -14,19 +15,19 @@ def handle_background(caller_type, person_id, caller_name, caller_value):
     if caller_type == CALLER_TYPES['EVENT']:
         print('starting event')
         p = PersonEvent.objects.create(person=person, event_name=caller_name, event_value=caller_value)
-        print(p.person.uid)
+        print('done ' + str(p.person.uid))
     elif caller_type == CALLER_TYPES['CONTACT']:
         print('starting contact')
         try:
             p = PersonContact.objects.create(person=person, contact_type=caller_name, contact_value=caller_value)
-            print('done ' + p.person.uid)
+            print('done ' + str(p.person.uid))
         except IntegrityError:
             print('fail, print debug')
     elif caller_type == CALLER_TYPES['DATA']:
         print('starting data')
         try:
             p = PersonData.objects.create(person=person, data_type=caller_name, data_value=caller_value)
-            print('done ' + p.person.uid)
+            print('done ' + str(p.person.uid))
         except IntegrityError:
             print('fail, print debug')
     else:
